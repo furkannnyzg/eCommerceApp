479df5d6fc3a427ba2951fe3d0531e82885f9aae
furkannnyzg <maxuxzone@gmail.com>
2024-09-23 22:13:07 +0300

    Proje Başlagıç
    Tailwind css ve proje kodları entegre edilmiştir.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       <h3 class="hover:bg-slate-600" >SELAM</h3>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
